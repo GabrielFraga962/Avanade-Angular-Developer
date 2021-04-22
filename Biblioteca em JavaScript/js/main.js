a75ae514ed33512d1eff31913c49b282c87bcc58
Gabriel S. Fraga <gabrielsouzafraga@outlook.com>
2021-04-21 21:36:17 -0300

    Exercício com algumas funções de JS

---
+++ Biblioteca em JavaScript/js/main.js
@@ -0,0 +1,56 @@
+// var nome = "Gabriel";
+// alert("Bem-Vindo de Volta! " + nome);
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Como começar o dia";
+// alert (nome + "tem " + idade "anos");
+// alert(idade + idade2);
+// console.long(nome);
+// console.long(n1 * n2);
+// console.long(frase.toLowerCase());
+// alert (frase.replace("Olá","Bom Dia"));
+
+
+// var lista = ["maça", "pêra", "laranja"];
+// lista.push("uva");
+// lista.pop();
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "))
+
+/*var fruta = {nome: "maça", cor:"vemelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+// var frutas = [{nome: "maça", cor:"vemelha"}, {nome: "uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
+// var idade = prompt("Qual sua idade"); // abre uma caixa de dialogo
+// if (idade >= 18) {
+//     alert("maior de idade");
+// }else{
+//     alert("menor de idade");
+// }
+  
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     alert(count)
+//     count ++;
+// }
+
+// var count;
+// for (count = 0; count <= 5; count++){
+//     alert(count);
+// }
+
+// var d = new Date();
+// alert(d.getFullYear());
+// alert(d.getMonth());
+// alert(d.getDate());
+// alert(d.getHours());
+// alert(d.getMinutes());
+// alert(d.getSeconds);
